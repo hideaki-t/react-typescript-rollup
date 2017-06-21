c5331d80420f8ad7e7b6f0073f7ba7976586d160
Hideaki Takahashi <mymelo@gmail.com>
2017-06-21 07:34:05 -0400

    replace "process" in src

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs'
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+
 export default {
   entry: './src/main.tsx',
   dest: './dest/main.js',
   format: 'iife',
 
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
     resolve({
       module: true,
     }),
