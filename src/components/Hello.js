692de81b063deda91e635cddb34448b0dddddd91
Hideaki Takahashi <mymelo@gmail.com>
2017-06-21 07:29:33 -0400

    remove generated file

--- src/components/Hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var React = require("react");
-exports.Hello = function (props) { return React.createElement("h1", null,
-    "Hello from ",
-    props.compiler,
-    " and ",
-    props.framework,
-    "!"); };
